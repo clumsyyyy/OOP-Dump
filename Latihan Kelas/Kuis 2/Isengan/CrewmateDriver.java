c3d8d44fa63059b7d14436231fa1549d69c16e88
clumsyyyy <owencwijaya@gmail.com>
2022-05-09 17:31:50 +0700

    cleaned sum stuf

--- Latihan Kelas/Kuis 2/Isengan/CrewmateDriver.java
+++
@@ -1,9 +0,0 @@
-public class CrewmateDriver {
-    public static void main(String[] args) {
-    Crewmate a = new Crewmate("Alpha");
-    Crewmate b = new Crewmate("Beta");
-    b.stab(a);
-    a.stab(b);
-    System.out.println(b.toString());
-  }
-}
--- Latihan Kelas/Kuis 2/Isengan/UtilityDriver.java
+++
@@ -1,9 +0,0 @@
-public class UtilityDriver {
-    public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4, 5};
-        System.out.println(Utility.join(arr));
-        System.out.println(Utility.join(Utility.repeat(arr, 3)));
-        System.out.println(Utility.sum(arr));
-        System.out.println(Utility.sum(Utility.repeat(arr, 3)));
-    }
-}
